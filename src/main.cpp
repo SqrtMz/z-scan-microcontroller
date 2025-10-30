9cc35f0df6ed38559e87ac14115120d22c3291d6
SqrtMz <cvesgaduran@gmail.com>
2025-10-30 17:48:37 -0500

    fixed go_to_start blocked on execute and added acceleration inputs

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,8 @@ int lim_switch_end_pin = 35;
 int photo_diode_pin = 33;
 float photo_diode_value = 0.0;
 
-float max_motor_speed = 25600;
+float max_motor_speed = 30000;
+float acceleration = 30000;
 
 char incoming_data[100];
 String commands[10];
@@ -23,6 +24,7 @@ Adafruit_ADS1115 ads;
 AccelStepper stepper(AccelStepper::DRIVER, pul_pin, dir_pin);
 
 bool is_moving = false;
+bool is_accelerated = true;
 
 float move_from, move_to, motor_speed, measure_separation, stabilization_time;
 
@@ -76,7 +78,8 @@ void setup() {
 	pinMode(lim_switch_end_pin, INPUT_PULLDOWN);
 
 	stepper.setMaxSpeed(max_motor_speed);
-	stepper.setAcceleration(10000.0);
+	stepper.setAcceleration(acceleration);
+	stepper.setSpeed(motor_speed);
 
 	ads.begin();
 	ads.setGain(GAIN_EIGHT);
@@ -99,7 +102,7 @@ void loop() {
 	}
 
 	if (commands[0] == "execute") {
-		if (stepper.currentPosition() != 0) { go_to_start(); }
+		if (stepper.currentPosition() != 0) { go_to_start(); delay(500);}
 		is_moving = true;
 
 		move_from = commands[1].toFloat();
@@ -107,6 +110,7 @@ void loop() {
 		motor_speed = ((commands[3].toFloat()) * 0.01) * max_motor_speed;
 		measure_separation = commands[4].toFloat();
 		stabilization_time = commands[5].toFloat();
+		// is_accelerated = commands[6].toInt();
 	}
 
 	else if (commands[0] == "stop") {
@@ -119,10 +123,11 @@ void loop() {
 	else if (commands[0] == "go_to_start") {go_to_start();}
 	else if (commands[0] == "go_to_end") {go_to_end();}
 	else if (commands[0] == "stop") {stop();}
-
+	
 	if (is_moving) {
 		
 		stepper.moveTo(move_from);
+		if (!is_accelerated) {stepper.setSpeed(motor_speed);}
 		stepper.run();
 
 		if (stepper.currentPosition() == move_from) {
@@ -134,14 +139,14 @@ void loop() {
 			photo_diode_value = analogRead(photo_diode_pin);
 			print_data();
 		}
-
-		if (stepper.currentPosition() >= move_to || digitalRead(lim_switch_start_pin) || digitalRead(lim_switch_end_pin)) {
-			stepper.stop();
-			delay(stabilization_time);
-			is_moving = false;
-		}
 	}
 
+	if (stepper.currentPosition() >= move_to || digitalRead(lim_switch_start_pin) || digitalRead(lim_switch_end_pin)) {
+		stepper.stop();
+		delay(stabilization_time);
+		is_moving = false;
+	}
+	
 	memset(incoming_data, '\0', sizeof(incoming_data));
 	memset(commands, '\0', sizeof(commands));
 }
