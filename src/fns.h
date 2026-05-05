2362b991aca39e948caa2153419a48fe7342ef7f
SqrtMz <cvesgaduran@gmail.com>
2026-05-05 14:37:01 -0500

    a little bit of refactor, added DEBUG flagvar and implemented the ADC

--- src/fns.h
+++ src/fns.h
@@ -1,68 +1,71 @@
 #include <Arduino.h>
 
+bool DEBUG = true;		// Changes the behaviour whether using the setup or just software debugging is needed
+
+int ENA_PIN = 25;		// Enable pin
+int DIR_PIN = 26;		// Direction pin
+int PUL_PIN = 27;		// Pulse pin
+
+int LS_START_PIN = 32;	// Limit switch start pin
+int LS_END_PIN = 35;	// Limit switch end pin
+int PD_PIN = 33;		// Photodiode pin
+
 void read_incoming_data(char *incoming_data, String *commands) {
 
 	int i = 0;
 	while (Serial.available()) {
-
 		char c = Serial.read();
-		
 		incoming_data[i] = c;
-
 		i++;
 	}
 
 	i = 0;
 	int j = 0;
 	while (true) {
-
 		char c = incoming_data[j];
 
 		if (c == '\0') {
-
 			break;
-			
 		} else if (c == ',') {
-
 			i++;
 			j++;
-
 		} else {
-
 			commands[i] += c;
-			
 			j++;
-
 		}
 
 	}
 }
 
-void print_data(float photo_diode_value, AccelStepper &stepper) {
+void print_data(float photo_diode_value, AccelStepper& stepper) {
 	Serial.print(photo_diode_value);
 	Serial.print(',');
 	Serial.println(stepper.currentPosition());
 }
 
-void go_to_start(int lim_switch_start_pin, int max_motor_speed, AccelStepper &stepper) {
+void go_to_start(int& lim_switch_start_pin, float& max_motor_speed, AccelStepper& stepper) {
 
-	while (!digitalRead(lim_switch_start_pin)) {
-		stepper.move(-100000);
-		stepper.setSpeed(-max_motor_speed);
-		stepper.run();
+	if (!DEBUG) {
+		while (!digitalRead(lim_switch_start_pin)) {
+			stepper.move(-100000);
+			stepper.setSpeed(-max_motor_speed);
+			stepper.run();
+		}
 	}
 
 	stepper.stop();
 	stepper.setCurrentPosition(0);
 }
 
-void go_to_end(int lim_switch_end_pin, int max_motor_speed, AccelStepper &stepper) {
+void go_to_end(int& lim_switch_end_pin, float& max_motor_speed, AccelStepper& stepper) {
 
-	while (!digitalRead(lim_switch_end_pin)) {
-		stepper.move(100000);
-		stepper.setSpeed(max_motor_speed);
-		stepper.run();
+	if (!DEBUG) {
+		while (!digitalRead(lim_switch_end_pin)) {
+			stepper.move(100000);
+			stepper.setSpeed(max_motor_speed);
+			stepper.run();
+		}
+	
+		stepper.stop();
 	}
-
-	stepper.stop();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,22 @@
 #include <Wire.h>
 #include "fns.h"
 
-int ena_pin = 25;
-int dir_pin = 26;
-int pul_pin = 27;
+float pd_value = 0.0; // Photodiode value
 
-int lim_switch_start_pin = 32;
-int lim_switch_end_pin = 35;
+float MAX_MOTOR_SPEED = 31000; // 32 - 6400
+// float MAX_MOTOR_SPEED = 14500; // 16 - 3200
+// float MAX_MOTOR_SPEED = 7500; // 8 - 1600
 
-int photo_diode_pin = 33;
-float photo_diode_value = 0.0;
+// float MAX_MOTOR_SPEED = 4500; // 4 - 800
+// float MAX_MOTOR_SPEED = 1850; // 2 - 400
 
-float max_motor_speed = 31000; // 32 - 6400
-// float max_motor_speed = 14500; // 16 - 3200
-// float max_motor_speed = 7500; // 8 - 1600
-
-// float max_motor_speed = 4500; // 4 - 800
-// float max_motor_speed = 1850; // 2 - 400
+// float MAX_MOTOR_SPEED[5] = {1850, 4500, 7500, 14500, 31000};
 
 char incoming_data[100];
 String commands[10];
 
-Adafruit_ADS1115 ads;
-AccelStepper stepper(AccelStepper::DRIVER, pul_pin, dir_pin);
+Adafruit_ADS1115 adc;
+AccelStepper stepper(AccelStepper::DRIVER, PUL_PIN, DIR_PIN);
 
 bool is_moving, is_accelerated;
 float move_from, move_to, measure_separation, motor_speed, stabilization_time;
@@ -33,25 +27,32 @@ float move_from, move_to, measure_separation, motor_speed, stabilization_time;
 void setup() {
 
 	Serial.begin(115200);
-	pinMode(photo_diode_pin, INPUT);
+	pinMode(PD_PIN, INPUT);
 
-	pinMode(dir_pin, OUTPUT);
-	pinMode(pul_pin, OUTPUT);
+	pinMode(DIR_PIN, OUTPUT);
+	pinMode(PUL_PIN, OUTPUT);
 
-	pinMode(ena_pin, OUTPUT);
-	digitalWrite(ena_pin, LOW); // LOW unlock - HIGH lock
+	pinMode(ENA_PIN, OUTPUT);
+	digitalWrite(ENA_PIN, LOW); // LOW unlock - HIGH lock
 
-	pinMode(lim_switch_start_pin, INPUT_PULLDOWN);
-	pinMode(lim_switch_end_pin, INPUT_PULLDOWN);
+	pinMode(LS_START_PIN, INPUT_PULLDOWN);
+	pinMode(LS_END_PIN, INPUT_PULLDOWN);
 
-	stepper.setMaxSpeed(max_motor_speed);
-	stepper.setAcceleration(max_motor_speed);
+	stepper.setMaxSpeed(MAX_MOTOR_SPEED);
+	stepper.setAcceleration(MAX_MOTOR_SPEED);
 	stepper.setSpeed(motor_speed);
 
-	ads.begin();
-	ads.setGain(GAIN_EIGHT);
+	// GAIN_TWOTHIRDS 	±6.144 V
+	// GAIN_ONE 		±4.096 V
+	// GAIN_TWO 		±2.048 V
+	// GAIN_FOUR 		±1.024 V
+	// GAIN_EIGHT 		±0.512 V
+	// GAIN_SIXTEEN 	±0.256 V
 
-	// go_to_start();
+	if (!adc.begin()) {Serial.println("ADC couldn't be initialized");}
+	adc.setGain(GAIN_TWOTHIRDS); 
+
+	if (!DEBUG) go_to_start(LS_START_PIN, MAX_MOTOR_SPEED, stepper);
 }
 
 void loop() {
@@ -59,26 +60,26 @@ void loop() {
 	if (Serial.available()) {read_incoming_data(incoming_data, commands);}
 
 	if (commands[0] == "execute") {
-		if (stepper.currentPosition() != 0) { go_to_start(lim_switch_start_pin, max_motor_speed, stepper); delay(1000);}
+		if (stepper.currentPosition() != 0) { go_to_start(LS_START_PIN, MAX_MOTOR_SPEED, stepper); delay(1000);}
 		is_moving = true;
 
-		move_from = commands[1].toFloat();
-		move_to = commands[2].toFloat();
-		motor_speed = commands[3].toFloat() * max_motor_speed * 0.01 * 0.15;
-		measure_separation = commands[4].toFloat();
-		stabilization_time = commands[5].toFloat();
-		is_accelerated = (bool)commands[6].toInt();
+		move_from = commands[1].toFloat();											// Receives start position in steps
+		move_to = commands[2].toFloat();											// Receives final position in steps
+		motor_speed = commands[3].toFloat() * MAX_MOTOR_SPEED * 0.01 * 0.15;		// Receives an int[1, 100]
+		measure_separation = commands[4].toFloat();									// Receives the separation where measures will be taken in steps
+		stabilization_time = commands[5].toFloat();									// Receives an int
+		is_accelerated = (bool)commands[6].toInt();									// Receives an int[0, 1]
 	}
 
 	else if (commands[0] == "stop") {
 		stepper.stop();
 		is_moving = false;
-		
+
 		Serial.println("Stopped");
 	}
 
-	else if (commands[0] == "go_to_start") {go_to_start(lim_switch_start_pin, max_motor_speed, stepper);}
-	else if (commands[0] == "go_to_end") {go_to_end(lim_switch_end_pin, max_motor_speed, stepper);}
+	else if (commands[0] == "go_to_start") {go_to_start(LS_START_PIN, MAX_MOTOR_SPEED, stepper);}
+	else if (commands[0] == "go_to_end") {go_to_end(LS_END_PIN, MAX_MOTOR_SPEED, stepper);}
 	else if (commands[0] == "stop") {stepper.stop();}
 
 	if (is_moving) {
@@ -90,23 +91,30 @@ void loop() {
 		if (stepper.currentPosition() == move_from) {
 			
 			if (stabilization_time != 0) {delay(stabilization_time);}
+
+			pd_value = adc.readADC_Differential_1_3();
+
+			// float pd_value = 0.0;
+			// for (size_t i = 0; i < 10; i++) {pd_value += adc.readADC_Differential_1_3();}
+			// pd_value /= 10;
+
+			Serial.println("===========================");
+			print_data(pd_value, stepper);
+
+			Serial.print("DIFF: ");
+			Serial.println(adc.computeVolts(pd_value));			
+
+			Serial.print("A1: ");
+			Serial.println(adc.computeVolts(adc.readADC_SingleEnded(1)));
+
+			Serial.print("A3: ");
+			Serial.println(adc.computeVolts(adc.readADC_SingleEnded(3)));
+
 			move_from = stepper.currentPosition() + measure_separation;
-			
-			// photo_diode_value = ads.readADC_Differential_0_1();
-			// photo_diode_value = ads.computeVolts(photo_diode_value);
-
-			float photo_diode_value = 0.0;
-			for (size_t i = 0; i < 10; i++)
-			{
-				photo_diode_value += analogRead(photo_diode_pin);
-			}
-			photo_diode_value /= 10;
-
-			print_data(photo_diode_value, stepper);
 		}
 	}
 
-	if (stepper.currentPosition() >= move_to || digitalRead(lim_switch_start_pin) || digitalRead(lim_switch_end_pin)) {
+	if (stepper.currentPosition() >= move_to || digitalRead(LS_START_PIN) || digitalRead(LS_END_PIN)) {
 		stepper.stop();
 		delay(stabilization_time);
 		is_moving = false;
