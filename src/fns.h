4c651eadd56910f8a78fd4963881f9fe82f939eb
SqrtMz <cvesgaduran@gmail.com>
2026-09-18 17:49:18 -0500

    stable but LS still blocking

--- src/fns.h
+++ src/fns.h
@@ -3,17 +3,18 @@
 #include <Arduino.h>
 #include <Adafruit_ADS1X15.h>
 
-bool NO_SETUP_DEBUG = false;				// Changes the behaviour whether using the setup or just software debugging is needed
-bool ADC_DEBUG = false;						// Enable printing information about the ADC pins
+bool NO_SETUP_DEBUG = false;	// Changes the behaviour whether using the setup or just software debugging is needed
+bool ADC_DEBUG = false;			// Enable printing information about the ADC pins
 
-int ENA_PIN = 16;				// Enable pin
-int DIR_PIN = 17;				// Direction pin
-int PUL_PIN = 5;				// Pulse pin
+int ENAN_PIN = 34;				// Enable - pin
+int ENAP_PIN = 35;				// Enable + pin
+int DIRN_PIN = 32;				// Direction - pin
+int DIRP_PIN = 33;				// Direction + pin
+int PULN_PIN = 25;				// Pulse - pin
+int PULP_PIN = 26;				// Pulse + pin
 
-int LS_START_PIN = 18;			// Limit switch start pin
-int LS_END_PIN = 19;			// Limit switch end pin
-
-int average_items = 1;			// Number of samples for average measurement of photodiodes
+int LS_START_PIN = 14;			// Limit switch start pin
+int LS_END_PIN = 27;			// Limit switch end pin
 
 adsGain_t ADC_GAIN_OPTION[6] = {GAIN_TWOTHIRDS, GAIN_ONE, GAIN_TWO, GAIN_FOUR, GAIN_EIGHT, GAIN_SIXTEEN};
 
@@ -24,18 +25,18 @@ adsGain_t ADC_GAIN_OPTION[6] = {GAIN_TWOTHIRDS, GAIN_ONE, GAIN_TWO, GAIN_FOUR, G
 // GAIN_EIGHT 		±0.512 V
 // GAIN_SIXTEEN 	±0.256 V
 
-// float MAX_MOTOR_SPEED = 31000; 		// 32 - 6400
-float MAX_MOTOR_SPEED = 14500; 	// 16 - 3200
-// float MAX_MOTOR_SPEED = 7500; 	// 8 - 1600
+int max_motor_speed = 1000;
 
-// float MAX_MOTOR_SPEED = 4500; 	// 4 - 800
-// float MAX_MOTOR_SPEED = 1850; 	// 2 - 400
-
-// float MAX_MOTOR_SPEED[5] = {1850, 4500, 7500, 14500, 31000};
+// int MAX_MOTOR_SPEED = 31000; 	// 32 - 6400
+// int MAX_MOTOR_SPEED = 15000; 	// 16 - 3200
+// int MAX_MOTOR_SPEED = 7500; 		// 8 - 1600
+// int MAX_MOTOR_SPEED = 4500; 		// 4 - 800
+// int MAX_MOTOR_SPEED = 1850; 		// 2 - 400
+// int MAX_MOTOR_SPEED = 1000; 		// 1 - 200
 
 enum SystemState {
 	IDLE,
-	RUNNING,
+	RUNNING
 };
 
 void read_incoming_data(char *incoming_data, String *commands) {
@@ -73,55 +74,28 @@ void print_data(float photo_diode_value1, float photo_diode_value2, AccelStepper
 }
 
 void go_to_start(AccelStepper& stepper) {
+
 	if (!NO_SETUP_DEBUG) {
-
-		bool start_was_pressed = digitalRead(LS_START_PIN);
-		bool end_was_pressed = digitalRead(LS_END_PIN);
-
-		while (true) {
-			stepper.setSpeed(-MAX_MOTOR_SPEED);
-			stepper.moveTo(-100000);
+		while (!digitalRead(LS_START_PIN)) {
+			stepper.move(-100000);
+			stepper.setSpeed(-max_motor_speed);
 			stepper.run();
-
-			bool start_pressed = digitalRead(LS_START_PIN);
-			bool end_pressed = digitalRead(LS_END_PIN);
-
-			bool new_press = (!start_was_pressed && start_pressed) || (!end_was_pressed && end_pressed);
-
-			if (new_press) break;
-
-			start_was_pressed = start_pressed;
-			end_was_pressed = end_pressed;
 		}
-
-		stepper.stop();
 	}
 
+	stepper.stop();
 	stepper.setCurrentPosition(0);
 }
 
 void go_to_end(AccelStepper& stepper) {
+
 	if (!NO_SETUP_DEBUG) {
-
-		bool start_was_pressed = digitalRead(LS_START_PIN);
-		bool end_was_pressed = digitalRead(LS_END_PIN);
-
-		while (true) {
-			stepper.setSpeed(MAX_MOTOR_SPEED);
-			stepper.moveTo(100000);
+		while (!digitalRead(LS_END_PIN)) {
+			stepper.move(100000);
+			stepper.setSpeed(max_motor_speed);
 			stepper.run();
-
-			bool start_pressed = digitalRead(LS_START_PIN);
-			bool end_pressed = digitalRead(LS_END_PIN);
-
-			bool new_press = (!start_was_pressed && start_pressed) || (!end_was_pressed && end_pressed);
-
-			if (new_press) break;
-
-			start_was_pressed = start_pressed;
-			end_was_pressed = end_pressed;
 		}
-	
+
 		stepper.stop();
 	}
 }
@@ -130,6 +104,8 @@ void print_adc_debug(Adafruit_ADS1115& adc) {
 
 	float phtd1_value = adc.readADC_Differential_1_3();
 	float phtd2_value = adc.readADC_Differential_2_3();
+
+	Serial.println("===========================");
 
 	Serial.print("DIFF 1 - 3: ");
 	Serial.println(adc.computeVolts(phtd1_value));
@@ -147,6 +123,9 @@ void print_adc_debug(Adafruit_ADS1115& adc) {
 	Serial.print("A3: ");
 	Serial.println(adc.computeVolts(adc.readADC_SingleEnded(3)));
 	
+	Serial.print("LS_START: ");
 	Serial.println(digitalRead(LS_START_PIN));
-	Serial.println("===========================");
+
+	Serial.print("LS_END  : ");
+	Serial.println(digitalRead(LS_END_PIN));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -12,59 +12,64 @@ char incoming_data[100];
 String commands[10];
 
 Adafruit_ADS1115 adc;
-AccelStepper stepper(AccelStepper::DRIVER, PUL_PIN, DIR_PIN);
+AccelStepper stepper(AccelStepper::DRIVER, PULP_PIN, DIRP_PIN);
+
+bool is_accelerated;
+int move_from, move_to, measure_separation, stabilization_time;
+float motor_speed;
+adsGain_t adc_gain;
+
+int average_items = 1;						// Number of samples for average measurement of photodiodes
 
 SystemState system_state = IDLE;
-bool is_accelerated = false;
-bool switch_pressed = false;
-float move_from, move_to, measure_separation, motor_speed, stabilization_time;
-adsGain_t adc_gain;
 
 void setup() {
 
 	Serial.begin(115200);
 
-	pinMode(DIR_PIN, OUTPUT);
-	pinMode(PUL_PIN, OUTPUT);
+	pinMode(DIRP_PIN, OUTPUT);
+	pinMode(PULP_PIN, OUTPUT);
 
-	pinMode(ENA_PIN, OUTPUT);
-	digitalWrite(ENA_PIN, LOW); // LOW unlock - HIGH lock
+	pinMode(ENAP_PIN, OUTPUT);
+	digitalWrite(ENAP_PIN, LOW); // LOW unlock - HIGH lock
+
+	pinMode(DIRN_PIN, OUTPUT);
+	pinMode(PULN_PIN, OUTPUT);
+	pinMode(ENAN_PIN, OUTPUT);
+	digitalWrite(DIRN_PIN, LOW);
+	digitalWrite(PULN_PIN, LOW);
+	digitalWrite(ENAN_PIN, LOW);
 
 	pinMode(LS_START_PIN, INPUT_PULLDOWN);
 	pinMode(LS_END_PIN, INPUT_PULLDOWN);
 
-	stepper.setMaxSpeed(MAX_MOTOR_SPEED);
-	stepper.setAcceleration(MAX_MOTOR_SPEED);
-	stepper.setSpeed(motor_speed);
+	stepper.setMaxSpeed(max_motor_speed);
+	stepper.setAcceleration(max_motor_speed / 3);
 
 	if (!adc.begin()) {Serial.println("ADC couldn't be initialized");}
 	adc.setGain(GAIN_TWOTHIRDS);
-
-	go_to_start(stepper);
 }
 
 void loop() {
 
-	if (Serial.available()) read_incoming_data(incoming_data, commands);
+	if (Serial.available()) { read_incoming_data(incoming_data, commands); }
 
 	if (commands[0] == "execute") {
-		if (stepper.currentPosition() != 0) {
-			go_to_start(stepper);
-			delay(1000);
-		}
-
+		if (stepper.currentPosition() != 0) go_to_start(stepper);
 		system_state = RUNNING;
 
-		move_from = commands[1].toFloat();											// Receives start position in steps
-		move_to = commands[2].toFloat();											// Receives final position in steps
-		motor_speed = commands[3].toFloat() * MAX_MOTOR_SPEED * 0.01;				// Receives an int[1, 100]
-		measure_separation = commands[4].toFloat();									// Receives the separation where measures will be taken in steps
-		stabilization_time = commands[5].toFloat();									// Receives an int
+		max_motor_speed = commands[9].toInt();										// Receives an int for the new motor_speed
+		move_from = commands[1].toInt();											// Receives start position in steps
+		move_to = commands[2].toInt();												// Receives final position in steps
+		motor_speed = commands[3].toFloat() * 0.01 * 0.15 * max_motor_speed;		// Receives an int[1, 100]
+		measure_separation = commands[4].toInt();									// Receives the separation where measures will be taken in steps
+		stabilization_time = commands[5].toInt();									// Receives an int
 		is_accelerated = (bool)commands[6].toInt();									// Receives an int[0, 1]
 		adc_gain = ADC_GAIN_OPTION[commands[7].toInt()];							// Receives an index for ADC_GAIN_OPTIONS[]
-		average_items = commands[8].toInt();
+		average_items = commands[8].toInt();										// Receives an int for the iterations on per measure
 
 		adc.setGain(adc_gain);
+		delay(1000);
 	}
 
 	else if (commands[0] == "stop") {
@@ -74,56 +79,67 @@ void loop() {
 		Serial.println("STOPPED");
 	}
 
-	else if (commands[0] == "go_to_start") go_to_start(stepper);
-	else if (commands[0] == "go_to_end") go_to_end(stepper);
+	else if (commands[0] == "go_to_start") {
+		max_motor_speed = commands[1].toInt();
+		stepper.setMaxSpeed(max_motor_speed);
+		stepper.setAcceleration(max_motor_speed / 4);
+		go_to_start(stepper);
+	}
 
-	switch (system_state) {
+	else if (commands[0] == "go_to_end") {
+		max_motor_speed = commands[1].toInt();
+		stepper.setMaxSpeed(max_motor_speed);
+		stepper.setAcceleration(max_motor_speed / 4);
+		go_to_end(stepper);
+	}
+
+	switch(system_state) {
 		case RUNNING:
+			if (!is_accelerated) stepper.setSpeed(motor_speed);
 			stepper.moveTo(move_from);
-			if (!is_accelerated && stepper.currentPosition() != 0) stepper.setSpeed(motor_speed);
-			stepper.run();
+			stepper.runSpeedToPosition();
 
 			if (stepper.currentPosition() == move_from) {
 				
 				if (stabilization_time != 0) delay(stabilization_time);
 
-				if (ADC_DEBUG) print_adc_debug(adc);
-				else {
+				pd_value = 0.0;
+				pd2_value = 0.0;
 
-					pd_value = 0.0;
-					pd2_value = 0.0;
-					for (size_t i = 0; i < average_items; i++) {
-						pd_value += adc.readADC_Differential_1_3();
-						pd2_value += adc.readADC_Differential_2_3();
-					}
-
-					pd_value /= average_items;
-					pd2_value /= average_items;
-
-					print_data(pd_value, pd2_value, stepper);
-
+				for (size_t i = 0; i < average_items; i++) {
+					pd_value += adc.readADC_Differential_1_3();
+					pd2_value += adc.readADC_Differential_2_3();
 				}
+
+				pd_value /= average_items;
+				pd2_value /= average_items;
+
+				print_data(pd_value, pd2_value, stepper);
 
 				move_from = stepper.currentPosition() + measure_separation;
 			}
-			
-			if (stepper.currentPosition() >= move_to) {
+
+			if (stepper.currentPosition() >= move_to || digitalRead(LS_START_PIN) || digitalRead(LS_END_PIN)) {
 				stepper.stop();
+				delay(stabilization_time);
 				system_state = IDLE;
 			}
 
-			if (!switch_pressed)
-				if (digitalRead(LS_START_PIN) || digitalRead(LS_END_PIN)) switch_pressed = true;
-			else {
-				stepper.stop();
-				system_state = IDLE;
-				switch_pressed = false;
-			}
-			
 			break;
 
 		case IDLE:
 		default:
+			if (ADC_DEBUG) {
+				print_adc_debug(adc);
+				for (String& c : commands) {
+						Serial.print(c);
+						Serial.print(",");
+				}
+				Serial.println("");
+				delay(100);
+			}
+			Serial.println("IDLE");
+			delay(100);
 			break;
 	}
 	
